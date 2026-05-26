d4383cb9d559a1f967312ff6a90b778e53f89f51
surajkhupse <surajkhupse22@gmail.com>
2026-05-26 14:06:31 +0530

    implment eslne and spell check

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import cspell from '@cspell/eslint-plugin'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -15,8 +16,35 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      '@cspell': cspell,
+    },
     languageOptions: {
       globals: globals.browser,
+    },
+    rules: {
+      '@cspell/spellchecker': ['warn', {
+        cspell: {
+          words: [
+            'reduxjs',
+            'hookform',
+            'tailwindcss',
+            'tseslint',
+            'orval',
+            'vitejs',
+            'axios',
+            'signin',
+            'signup',
+            'autofocus',
+            'novalidate',
+            'middleware',
+            'mui',
+            'zustand',
+            'prev',
+            'unmount',
+          ],
+        },
+      }],
     },
   },
 ])
